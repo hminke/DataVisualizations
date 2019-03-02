457508a470ec9f904c8b1df8a0677fd8cfa6479f
Heather Minke <heather.a.minke@gmail.com>
2019-03-02 13:44:09 -0700

    added legend and annotations to foodImports.py

--- Food Imports/foodImports.py
+++ Food Imports/foodImports.py
@@ -3,7 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 fig, ax, = plt.subplots()
-msg = ax.annotate('Click points for annotation', xy=(0, 0), xytext=(2, 20000))
+msg = ax.annotate('Click points for annotation', xy=(0, 0), xytext=(10, 20000))
+legendInfo = [None] * 15
+category = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '']
+dollar = 0
+index = 0
 
 with open("food_imports.csv", 'r') as fil:
     data = csv.DictReader(fil, delimiter=',')
@@ -14,19 +18,31 @@ with open("food_imports.csv", 'r') as fil:
         for year, dollarAmount in row.items():
             temp = dollarAmount.replace(',', '')
             if len(temp) != 0:
-                dollarAmount = float(temp)
-                ax.scatter(year, dollarAmount, color=color, picker=1, label=foodName)
+                dollar = float(temp)
+                if year == '1999':
+                    legendInfo[index] = ax.scatter(year, dollar, color=color, picker=1, label=foodName)
+                    category[index] = foodName
+                    index = index + 1
+                else:
+                    ax.scatter(year, dollar, color=color, picker=1, label=foodName)
 
 def onpick(event):
     global msg
-    category = event.artist.get_label()
+    categories = event.artist.get_label()
     x = event.artist.get_offsets()
-    dollar = int(x.item(0) + 1999)
+    amount = int(x.item(0) + 1999)
     msg.remove()
-    msg = ax.annotate("Category: {},\nYear: {},\n$ Million: {}\n".format(category, dollar, x.item(1)),
-                      xy=(0, 0), xytext=(2, 18000))
+    msg = ax.annotate("Category: {},\nYear: {},\n$ Million: {}\n".format(categories, amount, x.item(1)),
+                      xy=(0, 0), xytext=(4, 18000))
 
 plt.xlabel('Year')
 plt.ylabel('$ Millions')
+plt.legend((legendInfo[0], legendInfo[1], legendInfo[2], legendInfo[3], legendInfo[4],
+            legendInfo[5], legendInfo[6], legendInfo[7], legendInfo[8], legendInfo[9],
+            legendInfo[10], legendInfo[11], legendInfo[12], legendInfo[13], legendInfo[14]),
+           (category[0], category[1], category[2], category[3], category[4], category[5],
+            category[6], category[7], category[8], category[9], category[10], category[11],
+            category[12], category[13], category[14]), loc='upper left', bbox_to_anchor=(0., 1.15))
 fig.canvas.mpl_connect('pick_event', onpick)
+
 plt.show()
