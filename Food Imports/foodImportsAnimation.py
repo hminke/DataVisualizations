1adef626b5f7f2f1a64ef5c358501339be4ba591
Heather Minke <heather.a.minke@gmail.com>
2019-04-02 16:10:53 -0600

    Added a changing title to animation script. Cleaned up code.

--- Food Imports/foodImportsAnimation.py
+++ Food Imports/foodImportsAnimation.py
@@ -2,21 +2,33 @@ import csv
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from matplotlib.patches import Rectangle
 
-FRAME_DELTA = 700       # milliseconds
+FRAME_DELTA = 500       # milliseconds
 animationYear = 1999
 fig, ax, = plt.subplots()
-# animationTitle = ax.text(0.5, 0.85, "", transform=ax.transAxes, ha="center")
+animationTitle = ax.text(0.5, 0.85, "", transform=ax.transAxes, ha="center")
 index = 0
 N = 15
 ind = np.arange(N)
 width = 0.75
-animate = plt.bar(ind, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], width)
+animate01 = Rectangle((-.35, 0), width, 0)
+animate02 = Rectangle((.65, 0), width, 0)
+animate03 = Rectangle((1.65, 0), width, 0)
+animate04 = Rectangle((2.65, 0), width, 0)
+animate05 = Rectangle((3.65, 0), width, 0)
+animate06 = Rectangle((4.65, 0), width, 0)
+animate07 = Rectangle((5.65, 0), width, 0)
+animate08 = Rectangle((6.65, 0), width, 0)
+animate09 = Rectangle((7.65, 0), width, 0)
+animate10 = Rectangle((8.65, 0), width, 0)
+animate11 = Rectangle((9.65, 0), width, 0)
+animate12 = Rectangle((10.65, 0), width, 0)
+animate13 = Rectangle((11.65, 0), width, 0)
+animate14 = Rectangle((12.65, 0), width, 0)
+animate15 = Rectangle((13.65, 0), width, 0)
 years = np.zeros(19)
-dataYear = 0
-category = ''
 foodName = []
-plotCategory = []
 data99 = np.zeros(15)
 data00 = np.zeros(15)
 data01 = np.zeros(15)
@@ -110,25 +122,70 @@ dataSet = [data99, data00, data01, data02, data03, data04, data05, data06, data0
 def init():                     # init function for the animation
     ax.set_xlim(-1, 15)
     ax.set_ylim(0.0, 25000)
-    return animate
+    animate01.set_height(0)
+    animate02.set_height(0)
+    animate03.set_height(0)
+    animate04.set_height(0)
+    animate05.set_height(0)
+    animate06.set_height(0)
+    animate07.set_height(0)
+    animate08.set_height(0)
+    animate09.set_height(0)
+    animate10.set_height(0)
+    animate11.set_height(0)
+    animate12.set_height(0)
+    animate13.set_height(0)
+    animate14.set_height(0)
+    animate15.set_height(0)
+
+    ax.add_patch(animate01)
+    ax.add_patch(animate02)
+    ax.add_patch(animate03)
+    ax.add_patch(animate04)
+    ax.add_patch(animate05)
+    ax.add_patch(animate06)
+    ax.add_patch(animate07)
+    ax.add_patch(animate08)
+    ax.add_patch(animate09)
+    ax.add_patch(animate10)
+    ax.add_patch(animate11)
+    ax.add_patch(animate12)
+    ax.add_patch(animate13)
+    ax.add_patch(animate14)
+    ax.add_patch(animate15)
+
+    return animationTitle, animate01, animate02, animate03, animate04, animate05, animate06, animate07, animate08, \
+           animate09, animate10, animate11, animate12, animate13, animate14, animate15
 
 def update(price):
-    global animate
     global animationYear
-    # animationTitle.set_text('Food Imports for {}'.format(animationYear))
-    animate = ax.bar(ind, price, width)
-    # plt.draw()
-    animationYear = animationYear + 1
+    animationTitle.set_text('Food Imports for {}'.format(animationYear))
+    animate01.set_height(price[0])
+    animate02.set_height(price[1])
+    animate03.set_height(price[2])
+    animate04.set_height(price[3])
+    animate05.set_height(price[4])
+    animate06.set_height(price[5])
+    animate07.set_height(price[6])
+    animate08.set_height(price[7])
+    animate09.set_height(price[8])
+    animate10.set_height(price[9])
+    animate11.set_height(price[10])
+    animate12.set_height(price[11])
+    animate13.set_height(price[12])
+    animate14.set_height(price[13])
+    animate15.set_height(price[14])
+
+    animationYear += 1
     if animationYear > 2017:
         animationYear = 1999
-    return animate
+    return animationTitle, animate01, animate02, animate03, animate04, animate05, animate06, animate07, animate08, \
+           animate09, animate10, animate11, animate12, animate13, animate14, animate15
 
 ax.set_ylabel('Price ($) in Millions')
 ax.set_xticks(ind)
 ax.set_xticklabels(('Live\nmeat\nanimals', 'Meats', 'Fish\nand\nshellfish', 'Dairy', 'Vegies', 'Fruits', 'Nuts',
                     'Coffee,\ntea, and\nspices', 'Grains', 'Veg.\noils', 'Sugar\nand\ncandy',
                     'Cocoa\nand\nchoc.', 'Other\nedible\nprod.', 'Bev.', '  Liquors'))
-ax.set_title('Food Imports from 1999 - 2017')
-
 ani = animation.FuncAnimation(fig, update, frames=dataSet, init_func=init, interval=FRAME_DELTA, blit=True)
 plt.show()
